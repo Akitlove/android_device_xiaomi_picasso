3bfcc1bc645262aa8ab02a352fb13a6195ca7ad0
Akitlove <416613578@qq.com>
2025-06-30 11:45:10 +0800

    picasso: migrate TARGET_INIT_VENDOR_LIB warning: TARGET_INIT_VENDOR_LIB is deprecated, please migrate to soong_config_set,libinit,vendor_init_lib

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -18,7 +18,7 @@ TARGET_OTA_ASSERT_DEVICE := picasso,milito
 TARGET_SCREEN_DENSITY := 440
 
 # Init
-TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):init_xiaomi_picasso
+$(call soong_config_set,libinit,vendor_init_lib,//$(DEVICE_PATH):init_xiaomi_picasso)
 TARGET_RECOVERY_DEVICE_MODULES := init_xiaomi_picasso
 
 # Properties
