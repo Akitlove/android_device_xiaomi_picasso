f1e97bb726695c02682624081da67798f81b276e
Akitlove <416613578@qq.com>
2025-07-01 10:06:32 +0800

    picasso: fix lunch menu for BP1A

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -8,6 +8,6 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/lineage_picasso.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_picasso-ap2a-user \
-    lineage_picasso-ap2a-userdebug \
-    lineage_picasso-ap2a-eng
+    lineage_picasso-bp1a-user \
+    lineage_picasso-bp1a-userdebug \
+    lineage_picasso-bp1a-eng
