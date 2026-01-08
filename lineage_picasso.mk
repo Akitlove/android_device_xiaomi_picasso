b1d0829da1c49298d57dd80ce767545756cd177e
Akitlove <416613578@qq.com>
2026-01-09 06:55:48 +0800

    picasso: Switch to surfaceflinger content detection API This is to allow refresh rate switching to be truly dynamic based on the refresh rate requested by the app while allowing for us to add boost & idle cases in the future through further properties

--- lineage_picasso.mk
+++ lineage_picasso.mk
@@ -20,6 +20,31 @@ PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := Redmi
 PRODUCT_MODEL := Redmi K30 5G
 
+# Lunch banner maintainer variable
+RISING_MAINTAINER="Akitlove"
+
+# Chipset/Maintainer properties (ro.rising.chipset/ro.rising.maintainer)
+# Set RISING_MAINTAINER for version control
+# (Optional if builder is setting properties via init_<device>.cpp)
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    RisingChipset="Redmi K30 5G" \
+    RisingMaintainer="Akitlove"
+
+RISING_MAINTAINER := Akitlove
+
+# Disable/enable blur support, false by default
+TARGET_ENABLE_BLUR := true
+
+# Whether to ship aperture camera, false by default
+PRODUCT_NO_CAMERA := false
+
+# Whether to ship lawnchair launcher, false by default
+TARGET_PREBUILT_LAWNCHAIR_LAUNCHER := true
+
+# CORE build flags
+WITH_GMS := true
+TARGET_USES_PICO_GAPPS := true
+
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
