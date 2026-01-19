0e1d1101d866f5bd967c4db689a5727471e63573
Akitlove <416613578@qq.com>
2026-01-19 12:03:38 +0800

    picasso: fix prop system

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -23,7 +23,7 @@ TARGET_RECOVERY_DEVICE_MODULES := init_xiaomi_picasso
 
 # Properties
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
-TARGET_VENDOR_PROP += $(DEVICE_PATH)/system.prop
+TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
 
 # Inherit from the proprietary version
 include vendor/xiaomi/picasso/BoardConfigVendor.mk
