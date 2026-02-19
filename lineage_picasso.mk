d450697943449febc13c8d3423aaa64cf6564f1c
Akitlove <416613578@qq.com>
2026-02-19 18:41:53 +0800

    picasso: axion

--- lineage_picasso.mk
+++ lineage_picasso.mk
@@ -20,30 +20,54 @@ PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := Redmi
 PRODUCT_MODEL := Redmi K30 5G
 
-# Lunch banner maintainer variable
-RISING_MAINTAINER="Akitlove"
-
-# Chipset/Maintainer properties (ro.rising.chipset/ro.rising.maintainer)
-# Set RISING_MAINTAINER for version control
-# (Optional if builder is setting properties via init_<device>.cpp)
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    RisingChipset="Redmi K30 5G" \
-    RisingMaintainer="Akitlove"
-
-RISING_MAINTAINER := Akitlove
-
-# Disable/enable blur support, false by default
+TARGET_DISABLE_EPPE := true
+#Enable blur effects
 TARGET_ENABLE_BLUR := true
 
-# Whether to ship aperture camera, false by default
-PRODUCT_NO_CAMERA := false
+# Camera information (multiple sensors supported)
+AXION_CAMERA_REAR_INFO := 64/48,8,5,2
+AXION_CAMERA_FRONT_INFO := 20
 
-# Whether to ship lawnchair launcher, false by default
-TARGET_PREBUILT_LAWNCHAIR_LAUNCHER := true
+# Maintainer name (underscores become spaces in the UI)
+AXION_MAINTAINER := Akitlove
 
-# CORE build flags
-WITH_GMS := true
-TARGET_USES_PICO_GAPPS := true
+# Processor name (underscores become spaces)
+AXION_PROCESSOR := Snapdragon_765G_(7_nm)
+PERF_GOV_SUPPORTED := false
+PERF_DEFAULT_GOV := schedutil
+PERF_ANIM_OVERRIDE := false
+
+# GPU
+GPU_FREQS_PATH := /sys/class/kgsl/kgsl-3d0/freq_table_mhz
+GPU_MIN_FREQ_PATH := /sys/class/kgsl/kgsl-3d0/min_clock_mhz
+
+# Charging
+BYPASS_CHARGE_SUPPORTED := false
+
+# High Brightness Mode (HBM)
+HBM_SUPPORTED := false
+
+# Flashlight strength
+TORCH_STR_SUPPORTED := false
+
+# doze fix
+# for devices with doze/sensor related issues
+TARGET_NEEDS_DOZE_FIX := false
+
+# do not enable TARGET_IS_LOW_RAM if your device ram is greater than 4gb
+# else OOM will most likely occur on operations where applications and camera can fill heap limit
+# e.g uploading video/media on apps with camera preview
+# this is a mitigation targets legacy devices 4gb below
+# Using quicken is a trade-off: here we trade clean pages for dirty pages,
+# extra cpu and battery. That's because the quicken files will be jit-ed in all
+# the processes that load of shared apk and the code cache is not shared.
+# Some notable apps that will be affected by this are gms and chrome.
+TARGET_IS_LOW_RAM := false
+
+# refresh rate list (optional) - defining this flags makes the controller skip the parsing of Display.Mode refresh rates
+# for supported refresh rates. The refresh rate controller will also assume that all the defined refresh rates are "supported"
+TARGET_SUPPORTED_REFRESH_RATES := 30,50,60,90,120
+TARGET_TOUCH_BOOST_SUPPORTED := true
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
